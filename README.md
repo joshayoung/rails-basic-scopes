5dcfd2fa36772f841483078b24c7ab77d7edce53
Josh Young <j22cal@gmail.com>
2019-05-14 19:50:37 -0500

    Add seed file

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 Steps Used to Setup Code Base:
 * Create the model
-  * rails g model student first:string last:string age:string grade_average:string active:boolean last_attendance:date
+  * `rails g model student first:string last:string age:string grade_average:string active:boolean last_attendance:date`
 * Create the controller
-  * rails g controller students
+  * `rails g controller students`
 * Create a basic set of routes
 * Add a single index view file and controller action
+* Create and run seed file
+  * `rails db:seed`
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Student.destroy_all
+
+all_students = [
+  [ "James", "Franklin", "38", "81", "true", "2019-03-21" ],
+  [ "Sally", "Johnson", "23", "90", "false", "2019-04-03" ],
+  [ "Molly", "Crembal", "63", "92", "true", "2018-12-05" ],
+  [ "Tom", "Morris", "49", "76", "true", "2019-02-12" ],
+  [ "Kimberly", "Charleston", "19", "98", "false", "2017-09-08" ],
+  [ "Colton", "Grover", "52", "85", "false", "2019-05-14" ],
+  [ "Bob", "Anderson", "92", "75", "true", "2019-03-10" ]
+]
+
+all_students.each do |first, last, age, grade_average, active, last_attendance|
+  Student.create(
+    first: first,
+    last: last,
+    age: age,
+    grade_average: grade_average,
+    active: active,
+    last_attendance: last_attendance
+  )
+end
