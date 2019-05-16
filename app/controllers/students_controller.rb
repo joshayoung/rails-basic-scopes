595159895407269ff089794c3227ceeea06adc3a
Josh Young <j22cal@gmail.com>
2019-05-15 19:23:03 -0500

    Scope order

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,5 +3,6 @@ class StudentsController < ApplicationController
     @active_students = Student.active_students
     @active_students_scope = Student.passing
     @best_students_scope = Student.best_students
+    @order_by_last = Student.order_by_last
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 class Student < ApplicationRecord
   scope :active, -> { where(active: true) }
   scope :passing, -> { where("grade_average > 90") }
+  scope :order_by_last, -> { order('last') }
 
   def self.active_students
     where(active: true)
