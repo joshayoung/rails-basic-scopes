75365ce33453845c82d635d6617a91096cbbcf67
Josh Young <j22cal@gmail.com>
2019-05-15 19:17:55 -0500

    Scope chaining

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
   def index
     @active_students = Student.active_students
-    @active_students_scope = Student.active
+    @active_students_scope = Student.passing
+    @best_students_scope = Student.best_students
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,12 @@
 class Student < ApplicationRecord
   scope :active, -> { where(active: true) }
+  scope :passing, -> { where("grade_average > 90") }
 
   def self.active_students
     where(active: true)
+  end
+
+  def self.best_students
+    self.active.passing
   end
 end
