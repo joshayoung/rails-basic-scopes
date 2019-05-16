efdef9e6c630abc48d0d8f8692c77a499868cbc7
Josh Young <j22cal@gmail.com>
2019-05-15 19:34:54 -0500

    Pass a parameter to scope

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,6 @@ class StudentsController < ApplicationController
     @active_students_scope = Student.passing
     @best_students_scope = Student.best_students
     @order_by_last = Student.order_by_last
+    @last_name_eq_bosco = Student.find_last_name("Bosco")
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,7 @@ class Student < ApplicationRecord
   scope :active, -> { where(active: true) }
   scope :passing, -> { where("grade_average > 90") }
   scope :order_by_last, -> { order('last') }
+  scope :find_last_name, -> (last_name) { where(last: last_name) }
 
   def self.active_students
     where(active: true)
